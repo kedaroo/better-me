7d1f6a32cb8a3d35360d918bbf584fdba5e81fcf
Kedar Basutkar <117523152+kedar-int@users.noreply.github.com>
2023-01-21 00:52:09 +0530

    add signin page

--- client/src/pages/Signin/index.tsx
+++ client/src/pages/Signin/index.tsx
@@ -1,7 +1,28 @@
 import React from "react";
+import GoogleLogo from "../../assets/google.png";
+import "./index.css";
+import Logo from "../../assets/logo.png";
+import { useLogin } from "../../hooks/useLogin";
 
-const Signin = () => {
-  return <div>Signin</div>;
+const SignIn = () => {
+  const { login, error, isPending } = useLogin();
+
+  return (
+    <div className="signin">
+      <div className="wrapper card">
+        <img src={Logo} alt="" className="logo" />
+        <h1>Sign in</h1>
+        <p>to BetterMe</p>
+
+        <button onClick={login}>
+          <div className="logo-div">
+            <img src={GoogleLogo} alt="" />
+          </div>
+          <div>Continue with Google</div>
+        </button>
+      </div>
+    </div>
+  );
 };
 
-export default Signin;
+export default SignIn;
