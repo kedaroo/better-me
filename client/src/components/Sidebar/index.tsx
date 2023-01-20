b57c23e434b0de88f0b86c29f63aa713cf7f040d
Kedar Basutkar <117523152+kedar-int@users.noreply.github.com>
2023-01-21 00:32:09 +0530

    add siebar component

---
+++ client/src/components/Sidebar/index.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./index.css";
+import BreakIcon from "../../assets/sidebar/BreakIcon.png";
+import HomeIcon from "../../assets/sidebar/HomeIcon.png";
+import SleepIcon from "../../assets/sidebar/SleepIcon.png";
+import WaterIcon from "../../assets/sidebar/WaterIcon.png";
+import SignoutIcon from "../../assets/sidebar/SignoutIcon.png";
+
+const Sidebar = () => {
+  return (
+    <div className="sidebar">
+      <div className="link-wrapper">
+        <ul>
+          <li>
+            <img src={HomeIcon} alt="" />
+            <Link to="/home">Home</Link>
+          </li>
+          <li>
+            <img src={WaterIcon} alt="" />
+            <Link to="/WaterHabits">Water Habits</Link>
+          </li>
+          <li>
+            <img src={SleepIcon} alt="" />
+            <Link to="/SleepHabits">Sleep Habits</Link>
+          </li>
+          <li>
+            <img src={BreakIcon} alt="" />
+            <Link to="/BreakHabits">Break Habits</Link>
+          </li>
+          <li>
+            <img src={SignoutIcon} alt="" />
+            <button>Sign out</button>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
