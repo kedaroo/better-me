63689d87cae3ba12550e40722ab3c804393805de
Kedar Basutkar <117523152+kedar-int@users.noreply.github.com>
2023-01-21 00:32:30 +0530

    add routes in app compoennt

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,34 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { Route, Routes, Navigate } from "react-router-dom";
+import Home from "./pages/Home";
+import BreakHabits from "./pages/BreakHabits";
+import Landing from "./pages/Landing";
+import Signin from "./pages/Signin";
+import SleepHabits from "./pages/SleepHabits";
+import WaterHabits from "./pages/WaterHabits";
+import { useAuthContext } from "./hooks/useAuthContext";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { user, authIsReady } = useAuthContext();
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    <>
+      {authIsReady && (
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route
+            path="/signin"
+            element={user ? <Navigate to="/home" /> : <Signin />}
+          />
+          <Route path="/home" element={user ? <Home /> : <Navigate to="/" />} />
+          <Route
+            path="/BreakHabits"
+            element={user ? <BreakHabits /> : <Navigate to="/" />}
+          />
+          <Route
+            path="/SleepHabits"
+            element={user ? <SleepHabits /> : <Navigate to="/" />}
+          />
+          <Route
+            path="/WaterHabits"
+            element={user ? <WaterHabits /> : <Navigate to="/" />}
+          />
+        </Routes>
+      )}
+    </>
+  );
 }
 
-export default App
+export default App;
