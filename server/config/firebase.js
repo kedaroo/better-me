88fa7d13ef2b1ae214aed56b48345755c8fcafd8
Kedar Basutkar <117523152+kedar-int@users.noreply.github.com>
2023-01-21 12:36:30 +0530

    update firebase service account key

--- server/config/firebase.js
+++ server/config/firebase.js
@@ -1,5 +1,7 @@
 import admin from "firebase-admin";
-import serviceAccount from "./serviceAccountKey.json" assert { type: "json" };
+import { createRequire } from "node:module";
+const require = createRequire(import.meta.url);
+const serviceAccount = require("./serviceAccountKey.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
