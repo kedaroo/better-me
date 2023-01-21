7b7dd2318115bfe8978289a731b71df915fcf3bf
Kedar Basutkar <117523152+kedar-int@users.noreply.github.com>
2023-01-21 23:33:33 +0530

    feat: notification on interval

--- client/src/pages/Home/index.tsx
+++ client/src/pages/Home/index.tsx
@@ -1,12 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import SidebarLayout from "../../components/SidebarLayout";
 import { spawnNotification } from "../../helpers/notifications";
+import api from "../../api";
+import { useAuthContext } from "../../hooks/useAuthContext";
 
-const Home = () => {
+interface Props {
+  handleStartNotification: () => void;
+  appUser: any;
+  waterHabits: any;
+  sleepHabits: any;
+  breakHabits: any;
+}
+
+const Home = ({
+  handleStartNotification,
+  appUser,
+  waterHabits,
+  sleepHabits,
+  breakHabits,
+}: Props) => {
+  const { user, notificationIntervals } = useAuthContext();
+
+  const handleStopNotification = () => {
+    for (let i = 0; i < notificationIntervals.length; i++) {
+      clearInterval(notificationIntervals[i]);
+    }
+  };
+
   return (
     <SidebarLayout>
       <div>Home</div>
-      <button onClick={() => spawnNotification()}>Notify</button>
+      <button onClick={handleStartNotification}>Start Notifications</button>
+      <button onClick={handleStopNotification}>Stop Notifications</button>
     </SidebarLayout>
   );
 };
