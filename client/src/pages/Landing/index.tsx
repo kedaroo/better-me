6f66bf8a0a0cdcc1065f916ae9a5340708a62ace
DESKTOP-FJAKAS7\Siddhi <siddhigate16@gmail.com>
2023-01-21 20:00:08 +0530

    intermediate commit

--- client/src/pages/Landing/index.tsx
+++ client/src/pages/Landing/index.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { notify } from "../../helpers/notifications";
 
 const Landing = () => {
   return (
     <div>
       <Link to="/signin">Go to signin</Link>
+      <button onClick={notify}>Notify</button>
     </div>
   );
 };
