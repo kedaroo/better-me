b55e148cda58d9a040568930d93dc11b2d4b5b02
DESKTOP-FJAKAS7\Siddhi <siddhigate16@gmail.com>
2023-01-21 08:23:41 +0530

    feat: break habit get and delete by user id

--- server/routes/breakhabit.js
+++ server/routes/breakhabit.js
@@ -1,11 +1,12 @@
 import BreakhabitController from "../controllers/BreakhabitController.js";
 import express from "express";
+import { isAuthorized } from "../middlewares/auth.js";
 const router = express.Router();
 
 router
-  .route("/")
-  .get(BreakhabitController.getAll)
-  .post(BreakhabitController.insert);
+  .route("/user")
+  .get(isAuthorized, BreakhabitController.getByUserId)
+  .patch(isAuthorized, BreakhabitController.updateByUserId);
 
 router
   .route("/:id")
