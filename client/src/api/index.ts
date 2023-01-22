a8751eca5388a700eb08205da710854a23b109af
Kedar Basutkar <117523152+kedar-int@users.noreply.github.com>
2023-01-22 12:39:23 +0530

    use prod server

--- client/src/api/index.ts
+++ client/src/api/index.ts
@@ -1,5 +1,9 @@
 import axios from "axios";
 
+// export default axios.create({
+//   baseURL: `http://localhost:5000/api/v1/`,
+// });
+
 export default axios.create({
-  baseURL: `http://localhost:5000/api/v1/`,
+  baseURL: `https://better-me-server.onrender.com/api/v1/`,
 });
