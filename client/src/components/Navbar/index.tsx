3f03dbdc6897e5a9d0b9c91dfbfccb72f90ff4e9
Kedar Basutkar <117523152+kedar-int@users.noreply.github.com>
2023-01-22 11:00:40 +0530

    add link to navbar

--- client/src/components/Navbar/index.tsx
+++ client/src/components/Navbar/index.tsx
@@ -1,15 +1,16 @@
 import React from "react";
 import "./index.css";
 import Logo from "../../assets/logo.png";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <>
       <nav className="primary-nav">
-        <div>
+        <Link to="/">
           <img src={Logo} alt="Better Me" />
           <div>BetterMe</div>
-        </div>
+        </Link>
       </nav>
     </>
   );
